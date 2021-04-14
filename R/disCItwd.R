8cd3bc497d7468ecb28b2a32ddfd5f41fc7e3398
Ronja Foraita <foraita@leibniz-bips.de>
2021-04-14 10:17:14 +0200

    bug fixes in twd tests

--- R/disCItwd.R
+++ R/disCItwd.R
@@ -1,35 +1,35 @@
 #' G square Test for (Conditional) Independence between Discrete Variables with Missings
-#' 
-#' A wrapper for \code{pcalg::\link[pcalg]{disCItest}}, to be used within 
-#' \code{pcalg::\link[pcalg]{skeleton}}, \code{pcalg::\link[pcalg]{pc}} or 
-#' \code{pcalg::\link[pcalg]{fci}} when the data contain missing values. 
-#' Observations where at least one of the variables involved in the test is 
+#'
+#' A wrapper for \code{pcalg::\link[pcalg]{disCItest}}, to be used within
+#' \code{pcalg::\link[pcalg]{skeleton}}, \code{pcalg::\link[pcalg]{pc}} or
+#' \code{pcalg::\link[pcalg]{fci}} when the data contain missing values.
+#' Observations where at least one of the variables involved in the test is
 #' missing are deleted prior to performing the test (test-wise deletion).
 #'
-#' @param x,y,S  (integer) position of variable X, Y and set of variables S, 
-#'               respectively, in \code{suffStat}. It is tested whether X and Y 
-#'               are conditionally independent given the subset S of the remaining variables. 
-#'               
+#' @param x,y,S  (integer) position of variable X, Y and set of variables S,
+#'               respectively, in \code{suffStat}. It is tested whether X and Y
+#'               are conditionally independent given the subset S of the remaining variables.
+#'
 #' @param dm  data matrix (rows: samples, columns: variables) with integer entries; the k levels for a given column must be coded by the integers 0,1,...,k-1. (see example)
-#' 
+#'
 #' @param nlev  optional vector with numbers of levels for each variable in \code{dm}.
-#' 
+#'
 #' @param adaptDF  logical specifying if the degrees of freedom should be lowered by one for each zero count. The value for the degrees of freedom cannot go below 1.
-#'               
-#' @param suffStat   a list with three elements, \code{"dm"}, \code{"nlev"}, 
+#'
+#' @param suffStat   a list with three elements, \code{"dm"}, \code{"nlev"},
 #'                   \code{"adaptDF"}; each corresponding to the above arguments.
 #'
 #' @details
-#' See \code{\link[pcalg]{disCItest}} for details on the G square test. Test-wise deletion 
+#' See \code{\link[pcalg]{disCItest}} for details on the G square test. Test-wise deletion
 #' is valid if missingness does not jointly depend on X and Y.
-#' 
+#'
 #' @return A p-value.
-#' 
+#'
 #' @seealso \code{pcalg::\link[pcalg]{disCItest}} for complete data, \code{\link{disMItest}}
 #' for multiply imputed data
-#' 
+#'
 #' @examples
-#' 
+#'
 #' ## simulate variables (integers 0,...,k)
 #' n <- 200
 #' set.seed(123)
@@ -37,11 +37,11 @@
 #' y <- sample(0:3, n, TRUE) # 4 levels
 #' z <- sample(0:1, n, TRUE) # 2 levels
 #' dat <- cbind(x,y,z)
-#' 
+#'
 #' ## delete some observations of y and z
 #' dat[sample(1:n, 40), 2] <- NA
 #' dat[sample(1:n, 40), 3] <- NA
-#' 
+#'
 #' ## analyse incomplete data
 #' # test-wise deletion:
 #' suffStat <- list(dm = dat, nlev = c(3,4,2), adaptDF = FALSE)
@@ -49,26 +49,27 @@
 #' # list-wise deletion:
 #' suffStat2 <- list(dm = dat[complete.cases(dat), ], nlev = c(3,4,2), adaptDF = FALSE)
 #' disCItest(1, 3, NULL, suffStat = suffStat2)
-#' 
+#'
 #' @export
 
 
 disCItwd <- function(x, y, S=NULL, suffStat) {
+
   miss <- apply(suffStat$dm[, c(x, y, S)], 1, anyNA)
-  
+
   if (sum(!miss) < 2) { return(NA) }
-  
+
   if (length(unique(suffStat$dm[ ,1]))==1) {return(NA)}
   if (length(unique(suffStat$dm[ ,2]))==1) {return(NA)}
-  
+
   suffStat$dm <- suffStat$dm[!miss, c(x, y, S)]
   if (length(S) > 0) {
     S <- 3:ncol(suffStat$dm)
   }
-  
-  S1 <- apply(suffStat$dm[ ,S, drop=FALSE], 2, function(i){length(unique(i))==1})
+
+  S1 <- apply(suffStat$dm[ , S, drop = FALSE], 2, function(i){length(unique(i))==1})
   S <- S[!S1]
-  if (length(S)==0) {S <- NULL}
-  
+  if (length(S) == 0) {S <- NULL}
+
   gSquareDis(x = 1, y = 2, S = S, dm=suffStat$dm, adaptDF=suffStat$adaptDF, n.min=-1)
 }
--- R/mixCItwd.R
+++ R/mixCItwd.R
@@ -1,33 +1,33 @@
 #' Likelihood Ratio Test for (Conditional) Independence between Mixed Variables with Missings
-#' 
-#' A version of \code{\link{mixCItest}}, to be used within \code{pcalg::\link[pcalg]{skeleton}}, 
-#' \code{pcalg::\link[pcalg]{pc}} or \code{pcalg::\link[pcalg]{fci}} when the data contain missing values. 
-#' Observations where at least one of the variables involved in the test is missing 
+#'
+#' A version of \code{\link{mixCItest}}, to be used within \code{pcalg::\link[pcalg]{skeleton}},
+#' \code{pcalg::\link[pcalg]{pc}} or \code{pcalg::\link[pcalg]{fci}} when the data contain missing values.
+#' Observations where at least one of the variables involved in the test is missing
 #' are deleted prior to performing the test (test-wise deletion).
 #'
-#' @param x,y,S (integer) position of variable X, Y and set of variables S, 
-#' respectively, in \code{suffStat}. It is tested whether X and Y are conditionally 
+#' @param x,y,S (integer) position of variable X, Y and set of variables S,
+#' respectively, in \code{suffStat}. It is tested whether X and Y are conditionally
 #' independent given the subset S of the remaining variables.
 #' @param suffStat \code{data.frame}. Discrete variables must be coded as factors.
 #'
-#' @details See \code{\link{mixCItest}} for details on the assumptions of the 
-#' Conditional Gaussian likelihood ratio test. Test-wise deletion is valid if 
+#' @details See \code{\link{mixCItest}} for details on the assumptions of the
+#' Conditional Gaussian likelihood ratio test. Test-wise deletion is valid if
 #' missingness does not jointly depend on X and Y.
-#' 
+#'
 #' @return A p-value.
-#' 
-#' @seealso \code{\link{mixCItest}} for complete data, 
+#'
+#' @seealso \code{\link{mixCItest}} for complete data,
 #' \code{\link{mixMItest}} for multiply imputed data
-#' 
+#'
 #' @examples
 #' ## load data (numeric and factor variables)
 #' dat <- toenail2[1:400, ]
-#' 
+#'
 #' ## delete some observations
 #' set.seed(123)
 #' dat[sample(400, 20), 2] <- NA
 #' dat[sample(400, 30), 4] <- NA
-#' 
+#'
 #' ## analyse data
 #' # complete data:
 #' mixCItest(2, 3, 5, suffStat=toenail2[1:400, ])
@@ -36,19 +36,16 @@
 #' # list-wise deletion:
 #' suffStat <- dat[complete.cases(dat), ]
 #' mixCItest(2, 3, 5, suffStat = suffStat)
-#' 
+#'
 #' @export
 
 
 
-mixCItwd <- function(x, y, S=NULL, suffStat) {
-  
+mixCItwd <- function(x, y, S = NULL, suffStat) {
+
   miss <- apply(suffStat[ ,c(x,y,S)], 1, anyNA)
-  
+
   if (sum(!miss)==0) { return(NA) }
-  
-  if (length(S) > 0) {
-    S <- 3:(length(S)+2)
-  }
-  mixCItest(x = 1, y = 2, S = S, suffStat = suffStat[!miss, ])
+
+  mixCItest(x = x, y = y, S = S, suffStat = suffStat[!miss, ])
 }
