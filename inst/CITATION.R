412f797472f06b8a0cc9aeb5ea5c9e1a4750f84b
Janine Witte <jawitte@users.noreply.github.com>
2021-08-13 16:41:34 +0200

    added citation file

---
+++ inst/CITATION.R
@@ -0,0 +1,23 @@
+citHeader("To cite micd in publications, use:")
+
+citEntry(entry = "Article",
+  title        = "Causal discovery of gene regulation with incomplete data",
+  author       = personList(as.person("Ronja Foraita"),
+                   as.person("Juliane Friemel"),
+                   as.person("Kathrin G\\\"unther"),
+                   as.person("Thomas Behrens"),
+                   as.person("J\\\"orn Bullerdiek"),
+                   as.person("Rolf Nimzyk"),
+                   as.person("Wolfgang Ahrens"),
+                   as.person("Vanessa Didelez")),
+  journal      = "Journal of the Royal Statistical Society: Series A (Statistics in Society)",
+  year         = "2020",
+  volume       = "183",
+  number       = "4",
+  pages        = "1747--1775",
+
+  textVersion  =
+  paste("Ronja Foraita, Juliane Friemel, Kathrin Günther, Thomas Behrens, Jörn Bullerdiek, Rolf Nimzyk, Wolfgang Ahrens, Vanessa Didelez (2020).",
+        "Causal discovery of gene regulation with incomplete data.",
+        "Journal of the Royal Statistical Society: Series A (Statistics in Society), 183(4), 1747-1775.")
+)
